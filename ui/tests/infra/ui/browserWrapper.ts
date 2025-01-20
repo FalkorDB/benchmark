9b44bc37f7b66bc153688bab547f3b68ea0e369a
Naseem Ali <34807727+Naseem77@users.noreply.github.com>
2025-01-20 14:59:44 +0200

    implement infra

---
+++ ui/tests/infra/ui/browserWrapper.ts
@@ -0,0 +1,78 @@
+import { chromium, firefox,  Browser, BrowserContext, Page } from 'playwright';
+import BasePage from './basePage';
+import { test } from '@playwright/test';
+
+export default class BrowserWrapper {
+
+    private browser: Browser | null = null;
+
+    private context: BrowserContext | null = null;
+
+    private page: Page | null = null;
+
+    async createNewPage<T extends BasePage>(PageClass: new (page: Page) => T, url?: string) {
+        if (!this.browser) {
+            const projectName = test.info().project.name;
+            this.browser = await this.launchBrowser(projectName);
+        }
+        if (!this.context) {
+            this.context = await this.browser.newContext();
+        }
+        if (!this.page) {
+            this.page = await this.context.newPage();
+        }
+        if (url) {
+            await this.navigateTo(url)
+        }
+
+        const pageInstance = new PageClass(this.page);
+        return pageInstance;
+    }
+
+    private async launchBrowser(projectName: string): Promise<Browser> {
+        switch (projectName) {
+            case 'firefox':
+                return await firefox.launch();
+            case 'chromium':
+            default:
+                return await chromium.launch();
+        }
+    }
+
+    async getPage() {
+        if (!this.page) {
+            throw new Error('Browser is not launched yet!');
+        }
+        return this.page;
+    }
+
+    async setPageToFullScreen() {
+        if (!this.page) {
+            throw new Error('Browser is not launched yet!');
+        }
+        await this.page.setViewportSize({ width: 1920, height: 1080 });
+    }
+
+    async navigateTo(url: string) {
+        if (!this.page) {
+            throw new Error('Browser is not launched yet!');
+        }
+        await this.page.goto(url);
+        await this.page.waitForLoadState('networkidle');
+    }
+
+    async closePage() {
+        if (this.page) {
+            await this.page.close();
+        } else {
+            this.page = null;
+        }
+    }
+
+    async closeBrowser() {
+        if (this.browser) {
+            await this.browser.close();
+        }
+    }
+
+}
